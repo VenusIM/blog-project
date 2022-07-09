2a710597072039882bfa630ebb4e7af7fe9f6512
yim3370 <71271289+yim3370@users.noreply.github.com>
2022-07-09 19:47:58 +0900

    Initial commit

---
+++ src/main/java/blogPJT/BlogPjtApplication.java
@@ -0,0 +1,13 @@
+package blogPJT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogPjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blogPJT/BlogPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package blogPJT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
